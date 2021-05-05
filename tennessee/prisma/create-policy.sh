139ea0624b48b4051980d010b0db45c0c05d7ab3
jodydadescott <jody@thescottsweb.com>
2021-05-05 14:50:08 -0500

    new policies with new tags

--- tennessee/prisma/create-policy.sh
+++ tennessee/prisma/create-policy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -xe
-apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io import --file cluster.yaml
-apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/nashville import --file nashville.yaml
-apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/memphis import --file memphis.yaml
-apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/knoxville import --file knoxville.yaml
+apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io import --file kubedns-net.yaml
+apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io import --file kubedns-rule.yaml
+apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io import --file secops-rule.yaml
+apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/nashville import --file nashville-rule.yaml
+apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/memphis import --file memphis-rule.yaml
+apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/knoxville import --file knoxville-rule.yaml
