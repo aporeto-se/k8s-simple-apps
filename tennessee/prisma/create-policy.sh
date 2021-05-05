ad5a86821c254e079f1dfce8926b5dca71478fd4
jodydadescott <jody@thescottsweb.com>
2021-05-04 19:24:13 -0500

    added new tennessee

--- tennessee/prisma/create-policy.sh
+++ tennessee/prisma/create-policy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -xe
-apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io import --file config.yaml
+apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io import --file cluster.yaml
 apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/nashville import --file nashville.yaml
 apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/memphis import --file memphis.yaml
 apoctl api -n /806775361903163392/jody/k1-us-east-2-eksctl-io/knoxville import --file knoxville.yaml
