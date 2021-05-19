d267b0e9a323bc73a6c166ea96a95954760322b5
jodydadescott <jody@thescottsweb.com>
2021-05-19 10:02:52 -0500

    rewrite

---
+++ legacy/tennessee/k8s/restart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl delete pods -n nashville --all
+kubectl delete pods -n memphis --all
+kubectl delete pods -n knoxville --all
--- tennessee/k8s/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl delete pods -n nashville --all
-kubectl delete pods -n memphis --all
-kubectl delete pods -n knoxville --all
